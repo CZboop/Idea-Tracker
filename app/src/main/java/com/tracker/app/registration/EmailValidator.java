8f0bccd6c0c2cb127e6e85cabeef659bdc5daf35
Branchey <70140890+CZboop@users.noreply.github.com>
2021-12-28 16:42:04 +0000

    added email validator

---
+++ app/src/main/java/com/tracker/app/registration/EmailValidator.java
@@ -0,0 +1,15 @@
+package com.tracker.app.registration;
+
+import java.util.function.Predicate;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class EmailValidator implements Predicate<String> {
+
+    public boolean test(String email){
+        Pattern VALID_EMAIL_ADDRESS_REGEX =
+                Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = VALID_EMAIL_ADDRESS_REGEX.matcher(email);
+        return matcher.find();
+    }
+}
