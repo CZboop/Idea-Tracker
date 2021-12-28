6a25d454ae2508377839f32aebd4267988013f2d
Branchey <70140890+CZboop@users.noreply.github.com>
2021-12-28 15:55:12 +0000

    made service implement spring security interface, made dao transactional and added a method to dao to find user by email

--- app/src/main/java/com/tracker/app/user/UserDAO.java
+++ app/src/main/java/com/tracker/app/user/UserDAO.java
@@ -1,4 +1,14 @@
 package com.tracker.app.user;
 
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Repository
+@Transactional(readOnly = true)
 public interface UserDAO {
+
+    Optional<User> findByEmail(String email);
+
 }
--- app/src/main/java/com/tracker/app/user/UserService.java
+++ app/src/main/java/com/tracker/app/user/UserService.java
@@ -1,4 +1,15 @@
 package com.tracker.app.user;
 
-public class UserService {
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService implements UserDetailsService {
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
 }
