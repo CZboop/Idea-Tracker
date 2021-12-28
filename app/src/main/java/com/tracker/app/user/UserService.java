dad010ec7280f99941a1a1c8781feb01773d7056
Branchey <70140890+CZboop@users.noreply.github.com>
2021-12-28 16:02:03 +0000

    connected userdao to userservice and filled out overridden spring security method to find user in service

--- app/src/main/java/com/tracker/app/user/UserService.java
+++ app/src/main/java/com/tracker/app/user/UserService.java
@@ -1,15 +1,21 @@
 package com.tracker.app.user;
 
+import lombok.AllArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class UserService implements UserDetailsService {
 
+    private final UserDAO userDAO;
+    private final static String UserNotFoundMessage = "User with email %s not found";
+
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userDAO.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException(String.format(UserNotFoundMessage, email)));
     }
 }
