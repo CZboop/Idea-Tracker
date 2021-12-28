46390198f8679400a8c4221971a598b44f5abe14
Branchey <70140890+CZboop@users.noreply.github.com>
2021-12-28 22:27:08 +0000

    added password validator predicate

--- app/src/main/java/com/tracker/app/validation/PasswordValidator.java
+++ app/src/main/java/com/tracker/app/validation/PasswordValidator.java
@@ -1,4 +1,18 @@
 package com.tracker.app.validation;
 
-public class PasswordValidator {
+import org.springframework.stereotype.Service;
+
+import java.util.function.Predicate;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+@Service
+public class PasswordValidator implements Predicate<String> {
+
+    public boolean test(String password){
+        Pattern VALID_EMAIL_ADDRESS_REGEX =
+                Pattern.compile("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$");
+        Matcher matcher = VALID_EMAIL_ADDRESS_REGEX.matcher(password);
+        return matcher.find();
+    }
 }
