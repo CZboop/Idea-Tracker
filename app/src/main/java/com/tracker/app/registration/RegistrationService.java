c865cc0f68c2198d5662f7cd1efcd67855aa2636
Branchey <70140890+CZboop@users.noreply.github.com>
2021-12-28 16:27:03 +0000

    added security configurations and password encoder

--- app/src/main/java/com/tracker/app/registration/RegistrationService.java
+++ app/src/main/java/com/tracker/app/registration/RegistrationService.java
@@ -1,7 +1,10 @@
 package com.tracker.app.registration;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class RegistrationService {
     public String register(RegistrationRequest request) {
-        return "";
+        return "working";
     }
 }
