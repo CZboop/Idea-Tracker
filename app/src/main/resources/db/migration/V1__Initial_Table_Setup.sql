1e1bf3430e9c3cdc321d0de9fbb3e88b27d2816d
Branchey <70140890+CZboop@users.noreply.github.com>
2021-12-29 11:48:11 +0000

    changed database migrations to make user id a foreign key

--- app/src/main/resources/db/migration/V1__Initial_Table_Setup.sql
+++ app/src/main/resources/db/migration/V1__Initial_Table_Setup.sql
@@ -1,2 +1,5 @@
+CREATE TABLE IF NOT EXISTS users (id BIGSERIAL PRIMARY KEY, username TEXT,
+email TEXT, password TEXT, activated BOOLEAN);
+
 CREATE TABLE IF NOT EXISTS idea (id BIGSERIAL PRIMARY KEY, summary TEXT,
-details TEXT, priority TEXT, user_id INT);
+details TEXT, priority TEXT, user_id INT REFERENCES users(id) ON DELETE CASCADE);
--- app/src/main/resources/db/migration/V2__User_Table.sql
+++
@@ -1,2 +0,0 @@
-CREATE TABLE IF NOT EXISTS users (id BIGSERIAL PRIMARY KEY, username TEXT,
-email TEXT, password TEXT, activated BOOLEAN);
