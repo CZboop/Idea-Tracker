b6bf749f6bed268153baabaa108a982407e0ae72
Branchey <70140890+CZboop@users.noreply.github.com>
2021-12-30 12:35:21 +0000

    added method to convert an idea into a project

--- app/src/main/java/com/tracker/app/idea/IdeaService.java
+++ app/src/main/java/com/tracker/app/idea/IdeaService.java
@@ -1,6 +1,8 @@
 package com.tracker.app.idea;
 
-import com.tracker.app.exception.ResourceNotFound;
+import com.tracker.app.project.Project;
+import com.tracker.app.project.ProjectService;
+import com.tracker.app.project.Status;
 import com.tracker.app.user.UserService;
 import org.springframework.stereotype.Service;
 
@@ -11,10 +13,12 @@ public class IdeaService {
 
     private IdeaDAO ideaDAO;
     private UserService userService;
+    private ProjectService projectService;
 
-    public IdeaService(IdeaDAO ideaDAO, UserService userService) {
+    public IdeaService(IdeaDAO ideaDAO, UserService userService, ProjectService projectService) {
         this.ideaDAO = ideaDAO;
         this.userService = userService;
+        this.projectService = projectService;
     }
 
     public List<Idea> getAllIdeas() {
@@ -38,10 +42,18 @@ public class IdeaService {
     }
 
     public void makeProjectFromIdeaId(int id) {
-//        steps - get idea object from db
-//        - make project object from idea object
-//        - add project to database
-//        - delete idea from ideas db?
+        Idea idea = getIdeaById(id);
+        Project projectFromIdea = new Project(
+                null,
+                    Long.valueOf(idea.getUserId()),
+                    idea.getSummary(),
+                    idea.getDetails(),
+                    Status.ACTIVE,
+                    idea.getPriority(),
+                    null
+        );
+        projectService.addProject(projectFromIdea);
+        ideaDAO.deleteIdea(id);
     }
 
     public Idea getIdeaById(int id) {
