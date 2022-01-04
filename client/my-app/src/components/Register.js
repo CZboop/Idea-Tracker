de2a6edfa0ece31be5604f07f3728be8650a16f4
Branchey <70140890+CZboop@users.noreply.github.com>
2022-01-04 09:59:17 +0000

    added confirm password field when registering

--- client/my-app/src/components/Register.js
+++ client/my-app/src/components/Register.js
@@ -4,24 +4,31 @@ const Register = () => {
     const [username, setUsername] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        const newUser = {
-            "username": username,
-            "email": email,
-            "password": password
+        if (password != confirmPassword){
+            alert("Your passwords do not match, please try again")
         }
-        fetch("http://localhost:8080/api/user/signup", {
-                method: "POST",
-                headers: {
-                    'Content-type': 'application/json'
-                },
-                body: JSON.stringify(newUser)
-            })
-        setUsername("");
-        setEmail("");
-        setPassword("");
+        else{
+            const newUser = {
+                "username": username,
+                "email": email,
+                "password": password
+            }
+            fetch("http://localhost:8080/api/user/signup", {
+                    method: "POST",
+                    headers: {
+                        'Content-type': 'application/json'
+                    },
+                    body: JSON.stringify(newUser)
+                })
+            setUsername("");
+            setEmail("");
+            setPassword("");
+            setConfirmPassword("");
+        }
 
     }
 
@@ -32,6 +39,7 @@ const Register = () => {
                 <label>Username:</label><input type="text" value={username} onChange={(e) => setUsername(e.target.value)}></input>
                 <label>Email:</label><input type="text" value={email} onChange={(e) => setEmail(e.target.value)}></input>
                 <label>Password:</label><input type="password" value={password} onChange={(e) => setPassword(e.target.value)}></input>
+                <label>Confirm Password:</label><input type="password" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)}></input>
                 <input type="submit"/>
             </form>
 
