19ce60a2f488cb4164686174408c45eea5604b19
Branchey <70140890+CZboop@users.noreply.github.com>
2022-01-04 10:17:43 +0000

    started on login form and functionality in the front end

--- client/my-app/src/components/Login.js
+++ client/my-app/src/components/Login.js
@@ -1,7 +1,38 @@
+import { useState } from "react";
+
 const Login = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const userInfo = {
+            "email" : email,
+            "password" : password
+        }
+        fetch("http://localhost:8080/api/user/login", {
+                    method: "POST",
+                    headers: {
+                        'Content-type': 'application/json'
+                    },
+                    body: JSON.stringify(userInfo)
+                })
+                .then(response => response.json())
+                .then(data => {
+                    if (data==null){
+                        alert("User not found")
+                    }else{
+                    console.log(data.id)}})
+    }
+
     return (
         <div className="login-container">
             <h1>Login</h1>
+            <form className="login-form" onSubmit={handleSubmit}>
+                <label>Email:</label><input type="text" value={email} onChange={(e) => {setEmail(e.target.value)}}></input>
+                <label>Password:</label><input type="password" value={password} onChange={(e) => {setPassword(e.target.value)}}></input>
+                <input type="submit"></input>
+            </form>
 
         </div>
     )
